bd1c6b4357f4667f2c3fb729ecdf7402d65812f7
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-04-16 21:02:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click',function(){
+	let age=parseInt(document.getElementById("age").value);
+	let name=document.getElementById("name").value;
+	
+	// let prom = new Promise((resolve, reject)=>{
+	// 	setTimeout(()=>{
+	// 		if(age>18) resolve('y');
+	// 		else if(age<18) resolve('n');
+	// 	},4000)
+	// })
+
+	let prom  = new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			if(age>18){
+				resolve();
+			}
+			else{
+				reject();
+			}
+		},4000);
+	})
+
+	prom.then(()=> alert(`Welcome, ${name}. You can vote.`)).catch(()=>alert(`Oh sorry ${name}. You aren't old enough.`))
+	
+})
+
